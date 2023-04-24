3e19a734be4a312eb2cbc4c0808e67ebe80154df
oliverwang15 <oliverwang15@qq.com>
2023-04-25 02:02:13 +0800

    Updated on FinGPT-v1

---
+++ demos/FinGPT-v1/data_preparations/add_labels.py
@@ -0,0 +1,69 @@
+import sys
+sys.path.append("../../FinRL-Meta/")
+sys.path.append("D:/python_project/FinRL-Meta/FinRL-Meta")
+
+import os
+import datetime
+import pandas as pd
+from lxml import etree
+from tqdm import tqdm
+
+from meta.data_processors.akshare import Akshare
+from loguru import logger
+
+# The result_path should be the results with only titles which is the IN path
+result_path = "D:/python_project/FinRL-Meta/experiment/scrape/results_with_content/"
+
+# The base_path should be the results with labels which is the OUT path
+base_path = "D:/python_project/FinRL-Meta/experiment/scrape/content_with_labels/"
+file_list = os.listdir(result_path)
+
+def add_label(x, df_price, foward_days = 5, threshold = 0.02, threshold_very = 0.06):
+    publish_date = x.publish_date.strftime("%Y-%m-%d")
+    last_date = df_price[df_price.time < publish_date].iloc[-1].name
+    this_date_index = last_date + 1
+    next_date_index = this_date_index + foward_days
+    
+    if next_date_index >= df_price.shape[0]-1:
+        return "No data"
+    else:
+        this = df_price[df_price.index == this_date_index].open.values[0]
+        next_ = df_price[df_price.index == next_date_index].open.values[0]
+        change = (next_ - this)/this
+        if change > threshold_very:
+            return "very positive"
+        elif change > threshold:
+            return "positive"
+        elif change < -threshold_very:
+            return "very negative"
+        elif change < -threshold:
+            return "negative"
+        else:
+            return "neutral"
+
+@logger.catch()
+def process_label(file_name, foward_days = 5, threshold = 0.02, threshold_very = 0.06):
+    df = pd.read_csv(os.path.join(result_path, file_name))
+    df["post_publish_time"] = pd.to_datetime(df["post_publish_time"])
+    df["date"] = df["post_publish_time"].dt.date
+    df["time"] = df["post_publish_time"].dt.time
+    df["hour"] = df["post_publish_time"].dt.hour
+
+    start_date = df["date"].min() - datetime.timedelta(days = 10)
+    end_date = df["date"].max() + datetime.timedelta(days = 25)
+    start_date = start_date.strftime("%Y-%m-%d")
+    end_date = end_date.strftime("%Y-%m-%d")
+    code_list = [df.code_name.unique()[0]]
+
+    as_processor = Akshare("akshare", start_date = start_date, end_date= end_date, time_interval="daily")
+    as_processor.download_data(code_list, save_path = './data/dataset.csv')
+    df_price = as_processor.dataframe
+
+    df['publish_date'] = df.apply(lambda x:x['date'] if x['hour'] <15 else x['date'] + datetime.timedelta(days = 1), axis=1)
+    df["label"] = df.apply(lambda x:add_label(x, df_price = df_price, foward_days = foward_days, threshold = threshold, threshold_very = threshold_very), axis=1)
+    out_path = os.path.join(base_path, file_name)
+    df.to_csv(out_path, index = False)
+
+for id,file_name in enumerate(file_list):
+    print(id, file_name)
+    process_label(file_name)
---
+++ demos/FinGPT-v1/data_preparations/download_contents.py
@@ -0,0 +1,103 @@
+import os
+import pandas as pd
+import numpy as np
+import requests
+from lxml import etree
+import multiprocessing as mp
+import json
+
+# The result_path should be the results with only titles which is the IN path
+result_path = r"D:\python_project\FinRL-Meta\experiment\scrape\results"
+
+# The result_with_content_path should be the results with titles and contents which is the OUT path
+result_with_content_path = r"D:\python_project\FinRL-Meta\experiment\scrape\results_with_content"
+link_base = "https://guba.eastmoney.com"
+
+
+def get_one_content(x):
+    url = link_base + x["content link"]
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0",
+        "Referer": "https://guba.eastmoney.com/",
+    }
+
+    tunnel = YOUR_KUAIDAILI_TUNNEL
+    username = YOUR_KUAIDAILI_USERNAME
+    password = YOUR_KUAIDAILI_PASSWARD
+    proxies = {
+        "http": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": tunnel},
+        "https": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": tunnel}
+    }
+    requests.DEFAULT_RETRIES = 5  # more retrys
+    s = requests.session()
+    s.keep_alive = False  # close connection when finished
+    
+    ok = False
+    while not ok:
+        try:
+            response = requests.get(url = url, headers = headers, proxies= proxies)
+            if response == 200:
+                res = etree.HTML(response.text)
+                res = res.xpath("//script[1]//text()")[2][25:][:-136]
+                res = json.loads(res)
+                res = pd.Series(res["post"]).to_frame().T
+                ok = True
+                return res
+        except:
+            pass
+
+
+def get_content(file_name):
+    df = pd.read_csv(os.path.join(result_path, file_name))
+
+    new_columns = ['post_user', 'post_guba', 'post_publish_time', 'post_last_time',
+       'post_display_time', 'post_ip', 'post_checkState', 'post_click_count',
+       'post_forward_count', 'post_comment_count', 'post_comment_authority',
+       'post_like_count', 'post_is_like', 'post_is_collected', 'post_type',
+       'post_source_id', 'post_top_status', 'post_status', 'post_from',
+       'post_from_num', 'post_pdf_url', 'post_has_pic',
+       'has_pic_not_include_content', 'post_pic_url', 'source_post_id',
+       'source_post_state', 'source_post_user_id', 'source_post_user_nickname',
+       'source_post_user_type', 'source_post_user_is_majia',
+       'source_post_pic_url', 'source_post_title', 'source_post_content',
+       'source_post_abstract', 'source_post_ip', 'source_post_type',
+       'source_post_guba', 'post_video_url', 'source_post_video_url',
+       'source_post_source_id', 'code_name', 'product_type', 'v_user_code',
+       'source_click_count', 'source_comment_count', 'source_forward_count',
+       'source_publish_time', 'source_user_is_majia', 'ask_chairman_state',
+       'selected_post_code', 'selected_post_name', 'selected_relate_guba',
+       'ask_question', 'ask_answer', 'qa', 'fp_code', 'codepost_count',
+       'extend', 'post_pic_url2', 'source_post_pic_url2', 'relate_topic',
+       'source_extend', 'digest_type', 'source_post_atuser',
+       'post_inshare_count', 'repost_state', 'post_atuser', 'reptile_state',
+       'post_add_list', 'extend_version', 'post_add_time', 'post_modules',
+       'post_speccolumn', 'post_ip_address', 'source_post_ip_address',
+       'post_mod_time', 'post_mod_count', 'allow_likes_state',
+       'system_comment_authority', 'limit_reply_user_auth', 'post_id',
+       'post_title', 'post_content', 'post_abstract', 'post_state']
+    df[new_columns] = df.apply(lambda x:get_one_content(x), axis = 1, result_type= "expand", )
+    to_path = os.path.join(result_with_content_path, file_name)
+    df.to_csv(to_path, index = False)
+
+if __name__ == "__main__":
+    pool_list = []
+    res_list = []
+    pool = mp.Pool(processes = 12)
+    
+    file_list = os.listdir(result_path)
+    # get_content(file_list[0])
+
+    for i in file_list:
+        print(i)
+        res = pool.apply_async(get_content, args = (i,), error_callback = lambda x:print(x))
+        pool_list.append(res)
+
+    pool.close()
+    pool.join()
+
+    # # 获取运行结果
+    # for i in pool_list:
+    #     res_list.append(i.get())
+
+    print("All Done!")
