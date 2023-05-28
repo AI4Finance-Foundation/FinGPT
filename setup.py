901961e668ca51671a9cd3bc61e3153dc5dc19b3
Bruce Yang ByFinTech <hongyang.yang@columbia.edu>
2023-05-28 11:37:49 +0800

    2023-05-28 update

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt, ignore comments
+try:
+    REQUIRES = list()
+    f = open("requirements.txt", "rb")
+    for line in f.read().decode("utf-8").split("\n"):
+        line = line.strip()
+        if "#" in line:
+            line = line[: line.find("#")].strip()
+        if line:
+            REQUIRES.append(line)
+except:
+    print("'requirements.txt' not found!")
+    REQUIRES = list()
+
+setup(
+    name="FinGPT",
+    version="0.0.0",
+    include_package_data=True,
+    author="Hongyang Yang, Xiao-Yang Liu",
+    author_email="hy2500@columbia.edu",
+    url="https://github.com/AI4Finance-Foundation/FinGPT",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=REQUIRES,
+    description="FinGPT",
+    long_description="""FinGPT""",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="Financial Large Language Models",
+    platform=["any"],
+    python_requires=">=3.6",
+)
