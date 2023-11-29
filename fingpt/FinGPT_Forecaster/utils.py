8107665be5ecc12458895324177ceb87a518d0c4
Peter Schofield <pcschof@gmail.com>
2023-11-29 15:19:47 -0500

    Update utils.py
    reversed conditions for tmp_dataset so that datasets are loaded from disk if "if_remote" = True

--- fingpt/FinGPT_Forecaster/utils.py
+++ fingpt/FinGPT_Forecaster/utils.py
@@ -64,7 +64,7 @@ def load_dataset(names, from_remote=False):
         if not os.path.exists(name):
             rep = int(name.split('*')[1]) if '*' in name else 1
             name = ('FinGPT/fingpt-forecaster-' if from_remote else 'data/fingpt-forecaster-') + name.split('*')[0]
-        tmp_dataset = datasets.load_dataset(name) if from_remote else datasets.load_from_disk(name)
+        tmp_dataset = datasets.load_from_disk(name) if from_remote else datasets.load_dataset(name)
     
         if 'test' not in tmp_dataset:
             tmp_dataset = tmp_dataset.train_test_split(0.2, shuffle=True, seed=42)   
